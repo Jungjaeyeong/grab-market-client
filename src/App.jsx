e830f3d634e5162dd3812cf4d56e66e14b838e80
Jungjaeyeong <jjy8037@gmail.com>
2023-11-21 14:26:15 +0900

    grab-market

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MainPageComponent from "./main/index";
+
+const App = () => {
+  return (
+    <>
+      <MainPageComponent />
+    </>
+  );
+};
+
+export default App;
---
+++ src/main/index.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./index.css";
+
+const MainPage = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(function () {
+    axios
+      .get(
+        "https://341cf269-c712-4751-a587-2c7fd1b972ec.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러 발생 : ", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <div id="header">
+        <div id="header-area">
+          <img src="images/icons/logo.png" alt="대체이미지" />
+        </div>
+      </div>
+      <div id="body">
+        <div id="banner">
+          <img src="images/banners/banner1.png" alt="대체이미지" />
+        </div>
+        <h1>판매되는 상품들</h1>
+        <div id="product-list">
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <div>
+                  <img
+                    className="product-img"
+                    src={product.imageUrl}
+                    alt="대체이미지"
+                  />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <div className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                      alt="대체이미지"
+                    />
+                    <span>{product.seller}</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div id="footer"></div>
+    </>
+  );
+};
+
+export default MainPage;
