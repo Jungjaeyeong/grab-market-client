a659f8d8b3c9f11fd955236a1865b7fac19c4ba0
Jungjaeyeong <jjy8037@gmail.com>
2023-11-22 11:39:48 +0900

    상품 상세 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 import RouterConfig from "./RouterConfig";
+import "./App.css";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Suspense>
-          <RouterConfig />
+          <div id="header">
+            <div id="header-area">
+              <img src="/images/icons/logo.png" alt="대체이미지" />
+            </div>
+          </div>
+          <div id="body">
+            <RouterConfig />
+          </div>
+          <div id="footer"></div>
         </Suspense>
       </BrowserRouter>
     </>
